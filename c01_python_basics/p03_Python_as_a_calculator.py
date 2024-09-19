3aa7066943a08ecbe30d85313593543a59828d63
lmpescobar <lmpes@tutanota.com>
2024-09-19 15:24:29 -0400

    Python as a calculator

---
+++ c01_python_basics/p03_Python_as_a_calculator.py
@@ -0,0 +1,36 @@
+# Ejercicio: Python como calculadora
+# Python es ideal para realizar cálculos básicos. Puede hacer sumas, restas, multiplicaciones y divisiones.
+# El código en el script muestra algunos ejemplos.
+# ¡Ahora es tu turno de practicar!
+
+# Instrucciones:
+# 1. Imprime la suma de 4 + 5.
+# 2. Imprime el resultado de restar 5 a 5.
+# 3. Imprime el resultado de multiplicar 3 por 5.
+# 4. Imprime el resultado de dividir 10 entre 2.
+
+# Código en Python:
+
+# Addition
+print(4 + 5)  
+# La función print() se utiliza para mostrar en pantalla el resultado de la suma de 4 + 5.
+# El operador + realiza la adición de los dos números.
+# El resultado, 9, se imprime en la consola.
+
+# Subtraction
+print(5 - 5)  
+# La función print() muestra el resultado de restar 5 menos 5.
+# El operador - se utiliza para realizar la resta.
+# El resultado, 0, se imprime en la consola.
+
+# Multiplication
+print(3 * 5)  
+# La función print() muestra el resultado de la multiplicación de 3 por 5.
+# El operador * se utiliza para multiplicar los dos números.
+# El resultado, 15, se imprime en la consola.
+
+# Division
+print(10 / 2)  
+# La función print() muestra el resultado de dividir 10 entre 2.
+# El operador / realiza la división, siempre devolviendo un número flotante.
+# El resultado, 5.0, se imprime en la consola.

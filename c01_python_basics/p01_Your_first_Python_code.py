7ab384aeabd343074f32b33d92b5d383e1fcedf5
lmpescobar <lmpes@tutanota.com>
2024-09-19 11:04:59 -0400

    Your first Python code

---
+++ c01_python_basics/p01_Your_first_Python_code.py
@@ -0,0 +1,17 @@
+# Ejercicio
+# Tu primer código en Python
+# ¡Es momento de ejecutar tu primer código en Python!
+# Dirígete al código y presiona el botón "Run Code" para ver el resultado.
+
+# Instrucciones
+# 100 XP
+# Presiona el botón "Run Code" para ver el resultado de print(5 / 8).
+
+# Código en Python
+# La función print() se utiliza para mostrar el resultado de la operación 5 dividido entre 8.
+print(5 / 8)  # 5 dividido entre 8 es igual a 0.625.
+
+# Explicación del código:
+# - print(): Función que muestra en la consola el resultado de la expresión que se le pasa.
+# - 5 / 8: Operación matemática que divide 5 (numerador) entre 8 (denominador).
+#   - Resultado: 0.625, que es el valor impreso en la consola.

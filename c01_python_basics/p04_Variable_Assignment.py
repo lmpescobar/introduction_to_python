3aa70660aaf689c5bfc87b64754f9ca23422eb8d
lmpescobar <lmpes@tutanota.com>
2024-09-19 15:28:35 -0400

    Variable Assignment

---
+++ c01_python_basics/p04_Variable_Assignment.py
@@ -0,0 +1,22 @@
+# Ejercicio: Asignación de variables en Python
+# En Python, una variable te permite referirte a un valor con un nombre. Para crear una variable con un valor, 
+# se utiliza el operador de asignación (=). Por ejemplo, para asignar el valor 5 a una variable llamada x, 
+# escribirías: x = 5.
+# A partir de ese momento, puedes usar el nombre de la variable, x, en lugar del valor real, 5.
+
+# Instrucciones:
+# 1. Crea una variable llamada savings con el valor de 100.
+# 2. Imprime el valor de esta variable usando la función print(savings).
+
+# Código en Python:
+
+# Crear una variable llamada savings
+savings = 100  
+# La variable savings se inicializa con el valor 100.
+# Esto significa que cada vez que uses el nombre savings en el código, 
+# Python lo interpretará como el valor 100.
+
+# Imprimir el valor de savings
+print(savings)  
+# La función print() se utiliza para mostrar en pantalla el valor de la variable savings.
+# En este caso, imprimirá 100, que es el valor asignado previamente a la variable.

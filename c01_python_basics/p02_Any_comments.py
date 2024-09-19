96794181e417902be4563a0c5153a805c279f6c1
lmpescobar <lmpes@tutanota.com>
2024-09-19 15:14:05 -0400

    Any comments?

---
+++ c01_python_basics/p02_Any_comments.py
@@ -0,0 +1,19 @@
+# Ejercicio: ¿Algún comentario?
+# También puedes agregar comentarios a tus scripts en Python. Los comentarios son importantes para asegurarte 
+# de que tú y otros puedan entender de qué trata tu código y no se ejecutan como código de Python.
+
+# Comienzan con el símbolo #. Observa el comentario en el editor, # División; ¡ahora es tu turno de agregar un comentario!
+
+# Instrucciones
+# Reemplaza ____ con el comentario # Suma
+
+# Código en Python:
+# División
+print(5 / 8)  # Imprime el resultado de la división de 5 entre 8.
+
+# Suma
+print(7 + 10)  # Imprime el resultado de la suma de 7 más 10.
+
+# Explicación del código:
+# - En la primera línea se realiza una división entre 5 y 8, cuyo resultado es impreso en pantalla.
+# - En la segunda línea se realiza una suma entre 7 y 10, cuyo resultado también se imprime en pantalla.

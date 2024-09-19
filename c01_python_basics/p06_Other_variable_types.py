f7fca7e2f2f1f23379ee819a198833c1b0d2cd4d
lmpescobar <lmpes@tutanota.com>
2024-09-19 15:55:16 -0400

    Other variable types

---
+++ c01_python_basics/p06_Other_variable_types.py
@@ -0,0 +1,34 @@
+# Ejercicio
+# Otros tipos de variables
+
+# Descripción
+# En el ejercicio anterior, trabajaste con el tipo de dato entero (int) de Python:
+# - int, o entero: un número sin parte fraccionaria. 'savings', con el valor 100, es un ejemplo de un entero.
+
+# A continuación de los datos numéricos, existen otros tres tipos de datos muy comunes en Python:
+
+# 1. float, o punto flotante: un número que tiene tanto parte entera como fraccionaria, separado por un punto. 
+#    Por ejemplo, 1.1 es un ejemplo de un número flotante.
+# 2. str, o cadena de texto: un tipo para representar texto. Puedes usar comillas simples o dobles para construir una cadena.
+# 3. bool, o booleano: un tipo para representar valores lógicos. Solo puede ser True o False (¡la capitalización es importante!).
+
+# Instrucciones
+# 1. Crea un nuevo valor flotante (float) llamado 'half' con el valor 0.5.
+# 2. Crea una nueva cadena de texto (string) llamada 'intro' con el valor "Hello! How are you?".
+# 3. Crea un nuevo valor booleano (bool) llamado 'is_good' con el valor True.
+
+# Código en Python
+
+# Crear una variable 'half'
+half = 0.5  # Se asigna el valor flotante 0.5 a la variable 'half'.
+
+# Crear una variable 'intro'
+intro = "Hello! How are you?"  # Se asigna la cadena de texto a la variable 'intro'.
+
+# Crear una variable 'is_good'
+is_good = True  # Se asigna el valor booleano True a la variable 'is_good'.
+
+# Explicación del código:
+# - La variable 'half' almacena un número decimal o valor flotante.
+# - La variable 'intro' almacena un texto o cadena de caracteres.
+# - La variable 'is_good' almacena un valor booleano, que puede ser True o False.

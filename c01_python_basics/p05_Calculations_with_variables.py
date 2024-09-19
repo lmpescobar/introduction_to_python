371540a46be04954b6d6c97febcd60d9de18ef9e
lmpescobar <lmpes@tutanota.com>
2024-09-19 15:37:25 -0400

    Calculations with variables

---
+++ c01_python_basics/p05_Calculations_with_variables.py
@@ -0,0 +1,31 @@
+# Ejercicio: Cálculos con variables
+# Ahora que has creado una variable de ahorro, ¡vamos a empezar a ahorrar!
+# En lugar de calcular con valores reales, puedes usar variables.
+# ¿Cuánto dinero habrías ahorrado en cuatro meses si ahorras $10 cada mes?
+
+# Instrucciones:
+# 1. Crea una variable llamada monthly_savings con un valor de 10 y otra llamada num_months con un valor de 4.
+# 2. Multiplica monthly_savings por num_months y asigna el resultado a una nueva variable llamada new_savings.
+# 3. Imprime el valor de la variable new_savings.
+
+# Código en Python:
+
+# Crear las variables monthly_savings y num_months
+monthly_savings = 10  
+# Se crea una variable monthly_savings con el valor de 10, que representa la cantidad 
+# de dinero ahorrada cada mes.
+
+num_months = 4  
+# Se crea una variable num_months con el valor de 4, que representa el número de meses durante los 
+# cuales se ahorrará dinero.
+
+# Multiplicar monthly_savings por num_months
+new_savings = monthly_savings * num_months  
+# Se calcula el total ahorrado multiplicando el ahorro mensual (monthly_savings) por el número 
+# de meses (num_months).
+# El resultado, 40, se almacena en la variable new_savings.
+
+# Imprimir el valor de new_savings
+print(new_savings)  
+# La función print() se utiliza para mostrar en pantalla el valor de la variable new_savings.
+# En este caso, imprimirá 40, que es el total ahorrado en 4 meses si se ahorran $10 cada mes.

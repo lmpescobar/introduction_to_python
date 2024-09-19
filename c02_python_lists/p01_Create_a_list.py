5186feb8ee948c36c8637ef708a7dbac2a679fc9
lmpescobar <lmpes@tutanota.com>
2024-09-19 16:15:54 -0400

    Create a list

---
+++ c02_python_lists/p01_Create_a_list.py
@@ -0,0 +1,32 @@
+# Ejercicio
+# Crear una lista
+
+# Descripción
+# Una lista es un tipo de dato compuesto: puedes agrupar valores juntos, como esto:
+# a = "es"
+# b = "bonito"
+# mi_lista = ["mi", "lista", a, b]
+# Después de medir la altura de tu familia, decides recopilar algo de información 
+# sobre la casa en la que vives. Las áreas de las diferentes partes de tu casa 
+# se almacenan en variables separadas en este ejercicio.
+
+# Instrucciones
+# 1. Crea una lista llamada 'areas', que contenga el área del pasillo (hall), 
+#    la cocina (kit), la sala de estar (liv), el dormitorio (bed) y el baño (bath), 
+#    en este orden. Usa las variables predefinidas.
+# 2. Imprime 'areas' usando la función print().
+
+# Código en Python
+
+# Áreas de las habitaciones de la casa
+hall = 11.25  # Área del pasillo en metros cuadrados.
+kit = 18.0    # Área de la cocina en metros cuadrados.
+liv = 20.0    # Área de la sala de estar en metros cuadrados.
+bed = 10.75   # Área del dormitorio en metros cuadrados.
+bath = 9.50   # Área del baño en metros cuadrados.
+
+# Crear la lista 'areas'
+areas = [hall, kit, liv, bed, bath]  # Se crea una lista con las áreas en el orden especificado.
+
+# Imprimir la lista 'areas'
+print(areas)  # Se imprime la lista de áreas de las habitaciones.
